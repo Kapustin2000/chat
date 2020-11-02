21c78331456ee98e7bcfae11204afad0d48e7958
micle <Kapustin2000>
2020-11-02 20:18:07 +0200

    Setup required folders

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 'use strict';
 
-const express = require('express');
+import Express from 'express';
 
-// константы
 const port = 8080;
 const host = 'localhost';
 
-// приложение
-const app = express();
+const app = new Express();
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-
-app.listen(port);
-console.log(`running on http://${host}:${port}`);
