c901f30b2f2789c58726f9191f9548a427b54c80
micle <Kapustin2000>
2020-11-09 16:37:52 +0200

    finish setup project

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-// import './src/config/bootstrap';
+import './src/config/bootstrap.js';
 
 import DotEnv from 'dotenv'; DotEnv.config();
-
 import Express from 'express';
+import { router } from './src/config/router.js'
 
 const app = new Express();
 
 const HTTP_PORT = process.env['HTTP_PORT'];
 
-import { router } from '/src/config/router'
 
 app.listen(HTTP_PORT, () => {
     console.log(`running on port ${HTTP_PORT}`);
