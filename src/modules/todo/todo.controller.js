097937d6a07eec40680158ee31f5d0f5aa536ee7
micle <Kapustin2000>
2020-11-30 15:30:03 +0200

    refactor todo

--- src/modules/todo/todo.controller.js
+++ src/modules/todo/todo.controller.js
@@ -4,33 +4,35 @@ import { TodoService as Service } from 'src/modules/todo/services/todoService';
 import { TodoMiddleware } from 'src/modules/todo/todo.middleware';
 const router = Express.Router();
 
-router.get('/', async function (req, res, next) {
-    let todos = await Repository.get(req.payload.user._id);
-
-    res.json(todos);
+router.get('/', function (req, res, next) {
+     Repository.get(req.payload.user._id)
+        .then(todo => res.json(todo))
+        .catch(next);
 });
 
-router.post('/', async function (req, res, next) {
+router.post('/', (req, res, next) => {
     req.body.user_id = req.payload.user._id;
-    let todo = await Service.save(req.body);
-    res.json(todo);
+    Service.save(req.body)
+        .then(todo => res.json(todo))
+        .catch(next);
 });
 
-router.get('/:id', [TodoMiddleware], async function (req, res, next) {
-    let todo = await Repository.find(req.params.id, req.payload.user._id);
-
-    res.json(todo);
+router.get('/:id', [TodoMiddleware], (req, res, next) => {
+    Repository.find(req.params.id, req.payload.user._id)
+         .then(todo => res.json(todo))
+         .catch(next);
 });
 
-router.put('/:id', [TodoMiddleware], async function (req, res, next) {
-    let updated = await Service.update(req.body);
-    res.json(updated);
+router.put('/:id', [TodoMiddleware], (req, res, next) => {
+    Service.update(req.body)
+        .then(todo => res.json(todo))
+        .catch(next);
 });
 
-router.delete('/:id', [TodoMiddleware], async function (req, res, next) {
-    let deleted =  await Service.delete(req.body);
-
-    res.json(deleted);
+router.delete('/:id', [TodoMiddleware], (req, res, next) => {
+     Service.delete(req.body)
+        .then(todo => res.json(todo))
+        .catch(next);
 });
 
 export { router as TodoController};
