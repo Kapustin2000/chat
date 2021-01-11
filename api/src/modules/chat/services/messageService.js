fd0ada7120a0d870ff6b0436c82c0fac18f109fc
micle <Kapustin2000>
2021-01-11 13:31:44 +0200

    fix save message

--- api/src/modules/chat/services/messageService.js
+++ api/src/modules/chat/services/messageService.js
@@ -1,15 +1,15 @@
-import { JoinValidation } from 'src/modules/chat/services/validator';
+// import { JoinValidation } from 'src/modules/chat/services/validator';
 import { Message } from 'src/modules/chat/models/message.model';
 import { encrypt } from 'src/config/encryption';
 
 const MessageService = {
     async send(data) {
-
-       const { error } = JoinValidation(data);
-
-       if( error ) {
-           throw error.details[0].message;
-       }
+       //
+       // const { error } = JoinValidation(data);
+       //
+       // if( error ) {
+       //     throw error.details[0].message;
+       // }
 
        const { text, user_id, chat_id } = data;
 
