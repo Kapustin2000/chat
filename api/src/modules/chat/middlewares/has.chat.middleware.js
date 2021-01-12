123e8ed3d85e26f3300184d13c239b3b44834367
micle <Kapustin2000>
2021-01-12 16:54:43 +0200

    remove console.logs

--- api/src/modules/chat/middlewares/has.chat.middleware.js
+++ api/src/modules/chat/middlewares/has.chat.middleware.js
@@ -3,7 +3,6 @@ import { ChatRepository as Repository } from 'src/modules/chat/repositories/chat
 const HasChatMiddleware = function (req, res, next) {
     return Repository.find(req.payload.user._id)
         .then(chat => {
-            console.log(chat , chat._id.toString() , req.params.chat.toString(), chat._id.toString() === req.params.chat.toString());
             if(chat && chat._id.toString() === req.params.chat.toString()) {
                 return next()
             }
